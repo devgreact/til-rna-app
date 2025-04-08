c570a82fabba2517cdcea35c1c7be14cc4d3195e
devgreact <dev.greact@gmail.com>
2025-04-08 13:48:28 +0900

    component

--- src/screens/AboutScreen.tsx
+++ src/screens/AboutScreen.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {FlatList, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
 const AboutScreen = (): JSX.Element => {
+  const data = [
+    {id: 1, title: '사과'},
+    {id: 2, title: '딸기'},
+    {id: 3, title: '배'},
+    {id: 4, title: '참외'},
+  ];
+  const [refreshing, setRefreshing] = useState(false);
   return (
     <SafeAreaView style={styles.container}>
       <View>
-        <Text>About Screen</Text>
+        <FlatList
+          data={data}
+          renderItem={({item}) => (
+            <View style={{flex: 1, margin: 10}}>
+              <Text>{item.title}</Text>
+            </View>
+          )}
+          keyExtractor={item => item.id.toString()}
+          refreshing={refreshing}
+          onRefresh={() => {
+            setRefreshing(true);
+            setTimeout(() => setRefreshing(false), 2000);
+          }}
+        />
       </View>
     </SafeAreaView>
   );
@@ -14,7 +34,14 @@ const AboutScreen = (): JSX.Element => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'green',
+  },
+  viewStyle: {
+    flex: 1,
+  },
+  input: {
+    borderWidth: 1,
+    padding: 10,
+    margin: 5,
   },
 });
 export default AboutScreen;
